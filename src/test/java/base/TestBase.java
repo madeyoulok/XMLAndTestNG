a3bcef7d03e2a8f75fe09424f923d87ed4477f3d
nahid <nahid@DESKTOP-9IPN656.fios-router.home>
2020-05-17 17:54:17 -0400

    Commiting XmlAndTestNG to LocalBrance

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -63,13 +63,5 @@ public class TestBase {
 		return new ChangeDeliveryAction(driver);
 	}
 
-	public String getScreenShotPath(String testCaseName, WebDriver driver) throws IOException {
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		File source = ts.getScreenshotAs(OutputType.FILE);
-		String destinationFile = System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png";
-		FileUtils.copyFile(source, new File(destinationFile));
-		return destinationFile;
-
-	}
 
 }
